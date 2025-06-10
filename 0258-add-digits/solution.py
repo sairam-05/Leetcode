a482f14a3f09878bb7259bf181a7f78e141a4a15
sairam-05 <sainaidukonidela143@gmail.com>
2025-06-10 08:16:07 +0000

    Sync LeetCode submission Runtime - 1 ms (22.57%), Memory - 17.9 MB (100.00%)

--- 0258-add-digits/solution.py
+++ 0258-add-digits/solution.py
@@ -1,9 +1,10 @@
 class Solution:
     def addDigits(self, num: int) -> int:
+        
         while  num>=10:
             total=0
             for digit in str(num):
                 total+=int(digit)
             num=total
-        return num    
 
+        return num
