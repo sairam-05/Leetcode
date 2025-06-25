af41be1299b059a9037d2d9ce0ae41bdfde4325f
sairam-05 <sainaidukonidela143@gmail.com>
2025-06-25 08:18:56 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.4 MB (100.00%)

---
+++ 0136-single-number/solution.py
@@ -0,0 +1,6 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        result=nums[0]
+        for i in range(len(nums)-1):
+            result=result^nums[i+1]
+        return result
