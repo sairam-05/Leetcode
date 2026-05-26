ba084141c1972022cc4bbb1db18a4fef8ea1cad5
sairam-05 <sainaidukonidela143@gmail.com>
2026-05-26 05:02:56 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.2 MB (65.29%)

--- 0020-valid-parentheses/solution.py
+++ 0020-valid-parentheses/solution.py
@@ -1,18 +1,14 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        self.stack = []
-            # Map each opener to its matching closer
-        opener_to_closer = {'(': ')', '{': '}', '[': ']'}
-
-        for i in s:
-                if i in opener_to_closer:
-                    # Opening bracket → push its matching closing bracket
-                    self.stack.append(opener_to_closer[i])
-                else:
-                    # Closing bracket → must match top of stack
-                    if not self.stack or self.stack.pop() != i:
-                        return False
-
-            # At the end, no expected closers should be left
-        return not self.stack
-
+       st=[]
+       for i in s:
+        if i=="(":
+            st.append(")")
+        elif i=="[":
+            st.append("]")
+        elif i=="{":
+            st.append("}")
+        elif not st or st.pop()!=i:
+            return False
+       return not st
+            
