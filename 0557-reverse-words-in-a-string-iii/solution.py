3d3c76ffbbb37319ac1159138b16f284ef5b514a
sairam-05 <sainaidukonidela143@gmail.com>
2026-05-30 06:17:36 +0000

    Sync LeetCode submission Runtime - 3 ms (54.57%), Memory - 19.8 MB (25.02%)

--- 0557-reverse-words-in-a-string-iii/solution.py
+++ 0557-reverse-words-in-a-string-iii/solution.py
@@ -1,18 +1,8 @@
 class Solution:
     def reverseWords(self, s: str) -> str:
-        stack=[]
-        result=''
-        for i in s:
-            if i!=" ":
-                stack.append(i)
-            else:
-              while len(stack)!=0:
-                result+=stack.pop()
-              result+=i
-              
-        if len(stack)!=0:
-            while len(stack)!=0:
-                result+=stack.pop()
-        return result
-                
+        l=[]
+        n=s.split(" ")
+        for i in n:
+            l.append(i[::-1])
+        return " ".join(l)
 
