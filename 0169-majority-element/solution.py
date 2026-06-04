83122818c0938ce8bea09c7e771c134e9fe52936
sairam-05 <sainaidukonidela143@gmail.com>
2026-06-04 05:31:53 +0000

    Sync LeetCode submission Runtime - 12 ms (25.46%), Memory - 21.2 MB (49.69%)

--- 0169-majority-element/solution.py
+++ 0169-majority-element/solution.py
@@ -1,14 +1,15 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
+        res=[]
         n=len(nums)
-        t=(n+1)//2
-        freq={}
+        freq=dict()
         for num in nums:
             if num in freq:
                 freq[num]+=1
             else:
                 freq[num]=1
-        for k,v in freq.items():
-            if v>=t:
+        t=n//2
+        for (k,v) in freq.items():
+            if v>t:
                 return k
-        
+
