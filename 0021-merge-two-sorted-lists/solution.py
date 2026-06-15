d371bd5fac32d2185b6f62c66bc9f1ed30a95d03
sairam-05 <sainaidukonidela143@gmail.com>
2026-06-15 04:27:50 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.4 MB (6.15%)

---
+++ 0021-merge-two-sorted-lists/solution.py
@@ -0,0 +1,24 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        l3=ListNode()
+        ptr=l3
+        while list1 and list2:
+            if list1.val<=list2.val:
+                ptr.next=list1
+                list1=list1.next
+                ptr=ptr.next
+            else:
+                ptr.next=list2
+                list2=list2.next
+                ptr=ptr.next
+        if list1:
+                ptr.next=list1
+        if list2:
+                ptr.next=list2
+        return l3.next
+
