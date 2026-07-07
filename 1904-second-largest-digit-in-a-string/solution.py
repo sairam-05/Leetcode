fbe6e88eb5055bff39de1f792b4f1aabe1f98f38
sairam-05 <sainaidukonidela143@gmail.com>
2026-07-07 06:28:19 +0000

    Sync LeetCode submission Runtime - 7 ms (11.61%), Memory - 19.4 MB (8.42%)

--- 1904-second-largest-digit-in-a-string/solution.py
+++ 1904-second-largest-digit-in-a-string/solution.py
@@ -1,20 +1,20 @@
 class Solution:
     def secondHighest(self, s: str) -> int:
-        digits=""
+        second=-1
+        first=None
+        
+        num=None
         for i in s:
             if i.isdigit():
-                digits+=i+' '
-        digi=list(map(int,digits.split()))
-        max=-1
-        secondlargest=-1
-        for i in range(len(digi)):
-            if max<digi[i]:
-                secondlargest=max
-                max=digi[i]
-            if digi[i]>secondlargest and digi[i]!=max:
-                secondlargest=digi[i]
-        return secondlargest
-            
+                num=int(i)
+            if first==None:
+                first=num
 
-                
-
+            elif num<first:
+                second=max(second,num)
+            elif num>first:
+                temp=first
+                first=num
+                second=temp
+        return second
+    
